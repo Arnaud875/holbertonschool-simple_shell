84fc6094b7f0044bd6393478bd3c37e365496797
Arnaud875 <arnaud.nickles33@gmail.com>
2024-08-22 05:03:58 +0200

    Fix no-interactive exit code

--- input.c
+++ input.c
@@ -20,11 +20,11 @@ char *input()
 		if (buffer)
 			free(buffer);
 
-		if (feof(stdin))
-			exit(0);
-
 		if (!get_shell_instance()->is_interactive)
 			exit(get_shell_instance()->exit_code);
+
+		if (feof(stdin))
+			exit(0);
 
 		exit(EXIT_FAILURE);
 	}
